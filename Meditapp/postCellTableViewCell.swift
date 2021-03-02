c91721cd76b6f8cd8aa0bf136f84011c9cf90f61
gabbya8 <gabriellaalexis2399@gmail.com>
2021-03-02 03:34:26 -0500

    commit

---
+++ Meditapp/postCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  postCellTableViewCell.swift
+//  Meditapp
+//
+//  Created by Gabriella Alexis on 3/2/21.
+//
+
+import UIKit
+
+class postCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-Meditapp/Pods-Meditapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Meditapp : NSObject
+@end
+@implementation PodsDummy_Pods_Meditapp
+@end
