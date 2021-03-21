94642c5d3e2bc6705cba772fe0e7f3ba2383bff1
jackson.lu02@myhunter.cuny.edu <jackson.lu02@myhunter.cuny.edu>
2021-03-20 22:23:37 -0400

    pfp

--- Meditapp/UserProfilePageViewController.swift
+++ Meditapp/UserProfilePageViewController.swift
@@ -15,17 +15,36 @@ class UserProfilePageViewController: UIViewController {
     var pfpReference: StorageReference{
         return Storage.storage().reference().child("profilephotos")
     }
+    
     var postUser: User?
     
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var firstNameLabel: UILabel!
     @IBOutlet weak var lastNameLabel: UILabel!
-        
+    @IBOutlet weak var Pfp: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("in profile! uid: " + postUser!.uid)
+        loadPfp()
         // Do any additional setup after loading the view.
     }
+    
+    //TODO : CHECK IF USER HAS IMAGE: BOOL.
+    func loadPfp(){
+        let downloadImageRef = pfpReference.child("default.jpeg")
+        
+        let downloadTask = downloadImageRef.getData(maxSize: 1024 * 1024 * 12) { (data, error) in
+            if let data = data{
+                let image = UIImage(data: data)
+                self.Pfp.image = image
+            }
+            print(error ?? "NONE")
+        }
+        
+        downloadTask.resume()
+    }
+    
 }
